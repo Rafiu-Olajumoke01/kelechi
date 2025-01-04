c0b9ca548f5b44cf74782681bf01d00e826f2240
123rafiuolajumoke@gmail.com <123rafiuolajumoke@gmail.com>
2025-01-04 12:34:23 -0800

    kelechi

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,48 @@
 import React from 'react'
+import "./navbar.css"
+import { Link } from 'react-router-dom'
+import { MdSearch } from "react-icons/md";
+import { MdOutlineShoppingCart } from "react-icons/md";
 
 function Navbar() {
     return (
-        <div>
-            <div className="novakhayParent">
-                
-                <div className="logoItem">
-                    <div className="logo">
-                        <img src={require('./../Images/NovahkayLogo.jpg')} alt="" />
-                    </div>
-                </div>
+        <nav>
+            <div className="navbar_brand">
+                <img src={require("./../Images/NovahkayLogo-removebg-preview.png")} alt="" />
+            </div>
 
-                <div className="cartIcon">
+            <div className="pages">
+                <ul className="navbar_links">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
 
+                    <li>
+                        <Link to="/about">About Us</Link>
+                    </li>
+
+                    <li>
+                        <Link to="/products">Products</Link>
+                    </li>
+
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="cartItems">
+                <Link to="/cart">
+                    <MdOutlineShoppingCart size="24" />
+                </Link>
+                <span>0</span>
+
+
+                <div className="search">
+                    <MdSearch size={25} />
                 </div>
             </div>
-        </div>
+        </nav>
     )
 }
 
--- src/Pages/Home/HomeMain.jsx
+++ src/Pages/Home/HomeMain.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-
+import './home.css'
 function HomeMain() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <div className="container-fluid Banner">
+                <div className="row">
+                    <div className="col-md-6">
+                        <img src={require("./../../Images/asset3.jpg")} alt="" className='img-fluid' />
+                    </div>
+
+                    <div className="col-md-6">
+                        <img src={require("./../../Images/asset4.jpg")} alt="" className='img-fluid'/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default HomeMain
